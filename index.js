0333833f85a1941cc82bf5754649c4554d3e933d
nbriers32 <nathan.briers@tjmorris.co.uk>
2025-08-22 12:02:08 +0100

    animate ball left to right

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 
-let variable = "test";
-console.log(variable)
-
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let x = 100;
+let y = 100;
 
+const view = canvas.getContext("2d");
+view.fillStyle = "white";
+view.strokeStyle = "white";
+
+// view.moveTo(0, 0);
+// view.lineTo(100, 100);
+
+// view.fillRect(10, 10, 20, 20);
+
+let dx = 2;
+function animate() {
+    view.clearRect(0, 0, canvas.width, canvas.height);
+    x += dx;
+    view.beginPath();
+    view.arc(x, y, 50, 0, Math.PI * 2);
+    view.fill();
+    requestAnimationFrame(animate);
+}
+
+animate();
