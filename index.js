e93bd880fde3e0e856e714e8cde885598363bbc2
Riccardo <Riccardo.Foti@tjmorris.co.uk>
2025-08-29 12:02:39 +0100

    add gravity and bounce

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 
 const canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-let x = 100;
-let y = 100;
-
 const view = canvas.getContext("2d");
-view.fillStyle = "white";
-view.strokeStyle = "white";
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    view.fillStyle = "white";
+    view.strokeStyle = "white";
+}
+
+resize();
+addEventListener("resize", resize);
+let x = 400;
+let y = 400;
+let r = 50;
+
+
 
 // view.moveTo(0, 0);
 // view.lineTo(100, 100);
 
 // view.fillRect(10, 10, 20, 20);
-
-let dx = 2;
+let ax = 0;
+let ay = 0.01;
+let dx = 0;
+let dy = 0;
 function animate() {
     view.clearRect(0, 0, canvas.width, canvas.height);
+    dx += ax;
+    dy += ay;
     x += dx;
+    y += dy;
+    checkBounce();
     view.beginPath();
-    view.arc(x, y, 50, 0, Math.PI * 2);
+    view.arc(x, y, r, 0, Math.PI * 2);
     view.fill();
     requestAnimationFrame(animate);
+}
+
+function checkBounce() {
+    if (y + r >= canvas.height) {
+        y = canvas.height - r;
+        dy *= -0.9;
+    }
 }
 
 animate();
