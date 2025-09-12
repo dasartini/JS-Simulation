86e7f1b94c216acbcc8073df6c04c03efadfccaf
nbriers32 <nathan.briers@tjmorris.co.uk>
2025-09-12 12:10:30 +0100

    object orientated

--- index.js
+++ index.js
@@ -9,13 +9,54 @@ function resize() {
     view.strokeStyle = "white";
 }
 
+class Body {
+    constructor(x, y, r = 10){
+        this.x = x
+        this.y = y
+        this.radius = r
+        this.dx = Math.random() * 4 - 2
+        this.dy = Math.random() * 4 - 2
+    }
+    update(){
+        this.x += this.dx
+        this.y += this.dy
+        this.checkBounce()
+    }
+    draw(){
+        view.beginPath();
+        view.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        view.fill();
+    }
+    checkBounce(){
+        if (this.y + this.radius >= canvas.height){
+            this.y = canvas.height - this.radius;
+            this.dy *= -1;
+        }
+        if (this.x + this.radius >= canvas.width){
+            this.x = canvas.width - this.radius;
+            this.dx *= -1;
+        };
+        if (this.y - this.radius < 0){
+            this.y =  this.radius;
+            this.dy *= -1;
+        };
+        if (this.x - this.radius < 0){
+            this.x = this.radius;
+            this.dx *= -1;
+        };
+    }
+}
+
+const bodies = []
+for (let i = 0; i <= 100; i++){
+    bodies.push(new Body(400, 400, Math.random() * 20 + 5))
+}
+
 resize();
 addEventListener("resize", resize);
 let x = 400;
 let y = 200;
 let r = 50;
-
-
 
 // view.moveTo(0, 0);
 // view.lineTo(100, 100);
@@ -29,28 +70,32 @@ function animate() {
     view.clearRect(0, 0, canvas.width, canvas.height);
     // dx += ax;
     // dy += ay;
-    x += dx;
-    y += dy;
+    // x += dx;
+    // y += dy;
     // checkBounce();
-    checkWrap();
-    view.beginPath();
-    view.arc(x, y, r, 0, Math.PI * 2);
-    view.fill();
+    // checkWrap();
+    // view.beginPath();
+    // view.arc(x, y, r, 0, Math.PI * 2);
+    // view.fill();
+
+    for(const b of bodies) b.update();
+    for(const b of bodies) b.draw();
+
     requestAnimationFrame(animate);
 }
 
 function checkBounce() {
     if (y + r >= canvas.height){
         y = canvas.height - r;
-     dy *= -1;
+        dy *= -1;
     }
     if (x + r >= canvas.width){
         x = canvas.width - r;
         dx *= -1;
     };
-        if (y - r < 0){
+    if (y - r < 0){
         y =  r;
-     dy *= -1;
+        dy *= -1;
     };
     if (x - r < 0){
         x = r;
@@ -61,22 +106,19 @@ function checkBounce() {
 
 
 function checkWrap(){
-    if ( y - r >= canvas.height){
+    if (y - r >= canvas.height){
         y = -r;
     }
-    else
-      if ( x - r >= canvas.width){
+    else if (x - r >= canvas.width){
         x = -r;
     }
-    else
-    if (y + r <= 0 ){
+    else if (y + r <= 0 ){
         y = canvas.height + r
     }
-    else
-        if (x + r <= 0 ){
+    else if (x + r <= 0 ){
         x = canvas.width + r
     }
-
-
 }
 animate();
+
+
